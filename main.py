f360590cad7a12de60447bc227b9ec6ba8ad9765
Xiaoguang Li <47503491+Li-Xiaoguang@users.noreply.github.com>
2022-11-01 01:18:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -68,47 +68,29 @@ def main(mode=None):
         print('\nstart testing...\n')
         model.test()
 
-    # eval mode
-    else:
-        print('\nstart eval...\n')
-        model.eval()
-
 
 def load_config(mode=None):
 
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', '--checkpoints', type=str, default='./checkpoints', help='model checkpoints path (default: ./checkpoints)')
-    parser.add_argument('--model', type=int, choices=[1, 2, 3, 4], help='')
 
     args = parser.parse_args()
     config_path = os.path.join(args.path, 'config.yml')
 
-    # create checkpoints path if does't exist
     if not os.path.exists(args.path):
         os.makedirs(args.path)
 
-    # copy config template if does't exist
-    if not os.path.exists(config_path):
-        copyfile('./config.yml.example', config_path)
-
-    # load config file
     config = Config(config_path)
 
     # train mode
     if mode == 1:
         config.MODE = 1
-        if args.model:
-            config.MODEL = args.model
 
     # test mode
     elif mode == 2:
         config.MODE = 2
 
-    # eval mode
-    elif mode == 3:
-        config.MODE = 3
-        config.MODEL = args.model if args.model is not None else 3
 
     return config
 
